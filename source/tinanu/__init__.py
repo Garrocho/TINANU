86dbf04240ca5b4530abdf2d3b7ce77a9f87f35c
CharlesGarrocho <charles.garrocho@gmail.com>
2013-03-02 13:55:03 -0300

    adicionado: arquivo padrão do pacote do jogo tinanu.

---
+++ source/tinanu/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @author: Charles Tim Batista Garrocho
+# @contact: ccharles.garrocho@gmail.com
+# @copyright: (C) 2013 - 2013 Python Software Open Source
+
+"""
+Este é o pacote do projeto TINANU.
+"""
+
+
+__author__  = 'Charles Tim Batista Garrocho'
+
+__version__ = '0.1.0'
+
+__all__ = [
+    'app.py',
+    'atores.py',
+    'settings.py',
+]
