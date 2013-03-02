b4a85ac9f041f2f9810738bbea8acb3aad135b93
CharlesGarrocho <charles.garrocho@gmail.com>
2013-03-02 14:19:16 -0300

    adicionado: path das imagens e sons do jogo.

--- source/tinanu/settings.py
+++ source/tinanu/settings.py
@@ -8,5 +8,29 @@ Este é o módulo responsável pelas configurações do jogo, como path das imag
 """
 
 
+# Path das imagens do jogo.
 PATH_IMAGENS = '../imagens/'
-PATH_SONS    = '../sons/'
+
+# Path das imagens das naves.
+IMG_NAVE_STATUS = "../imagens/nave_status.png"
+IMG_NAVE_JOGADOR = "../imagens/nave.png"
+IMG_NAVE_INIMIGO = "../imagens/inimigo.png"
+
+# Path das imagens dos tiros.
+IMG_TIRO_JOGADOR = "../imagens/tiro.png"
+IMG_TIRO_INIMIGO = "../imagens/tiro_inimigo.png"
+
+# Path das imagens tile (background).
+IMG_TILE_1 = "../imagens/tile.jpg"
+IMG_TILE_2 = "../imagens/tile2.jpg"
+IMG_TILE_3 = "../imagens/tile3.jpg"
+IMG_TILE_4 = "../imagens/tile4.jpg"
+
+# Path dos sons do jogo.
+PATH_SONS = '../sons/'
+
+# Path dos tiros.
+SONS_TIRO = '../sons/tiro.wav'
+
+# Path das explosoes.
+SONS_EXPLOSAO = '../sons/explosao.wav'
