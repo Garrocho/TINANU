7f6637ea8c68cf8bc631cef962aa446d2d80c880
CharlesGarrocho <charles.garrocho@gmail.com>
2013-03-02 13:55:40 -0300

    adicionado: arquivos de configurações principais.

---
+++ source/tinanu/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @author: Charles Tim Batista Garrocho
+# @contact: ccharles.garrocho@gmail.com
+# @copyright: (C) 2013 - 2013 Python Software Open Source
+
+"""
+Este é o módulo responsável pelas configurações do jogo, como path das imagens, sons, etc.
+"""
+
+
+PATH_IMAGENS = '../imagens/'
+PATH_SONS    = '../sons/'
