c42489ffe929d5893d3b911e5344b3b9f45fdb21
CharlesGarrocho <charles.garrocho@gmail.com>
2013-07-30 10:34:14 -0300

    removido: carregamento de imagens e sons no construtor dos atores. #5

--- source/tinanu/atores.py
+++ source/tinanu/atores.py
@@ -7,8 +7,6 @@
 Este é o módulo responsável pelos atores do jogo, como background, jogador, inimigos, etc.
 """
 
-
-import media
 import pygame
 import settings
 from math import ceil
@@ -21,8 +19,7 @@ class Background:
     """
     image = None
 
-    def __init__( self, end_imagem = media.carrega_imagem(settings.IMG_TILE_1) ):
-        image = pygame.image.load( end_imagem )
+    def __init__( self, image ):
         self.isize  = image.get_size()
         self.pos    = [ 0, -1 * self.isize[ 1 ] ]
         screen      = pygame.display.get_surface()
@@ -38,18 +35,14 @@ class Background:
                 back.blit( image, ( i * self.isize[ 0 ], j * self.isize[ 1 ] ) )
 
         self.image = back
-    # __init__()
 
     def update( self, dt ):
         self.pos[ 1 ] += 1
         if ( self.pos[ 1 ] > 0 ):
             self.pos[ 1 ] -= self.isize[ 1 ]
-    # update()
 
     def draw( self, screen ):
         screen.blit( self.image, self.pos )
-    # draw()
-# Background
 
 
 class GameObject( pygame.sprite.Sprite ):
@@ -67,7 +60,6 @@ class GameObject( pygame.sprite.Sprite ):
         
         self.set_posicao( posicao )
         self.set_velocidade( velocidade or ( 0, 2 ) )
-    # __init__()
 
     def update( self, dt ):
         velocidade_mov = ( self.velocidade[ 0 ] * dt / 16, self.velocidade[ 1 ] * dt / 16 )
@@ -76,28 +68,21 @@ class GameObject( pygame.sprite.Sprite ):
             self.kill()
         if self.rect.bottom < -40:
             self.kill()
-    # update()
 
     def get_velocidade( self ):
         return self.velocidade
-    # get_velocidade()
 
     def set_velocidade( self, velocidade ):
         self.velocidade = velocidade
-    # set_velocidade()
 
     def get_posicao( self ):
         return ( self.rect.center[ 0 ], self.rect.bottom )
-    # get_posicao()
 
     def set_posicao( self, posicao ):
         self.rect.center = ( posicao[ 0 ], posicao[ 1 ] )
-    # set_posicao()
 
     def get_size( self ):
         return self.image.get_size()
-    # get_size()
-# GameObject
 
 
 class Tiro( GameObject ):
@@ -105,34 +90,28 @@ class Tiro( GameObject ):
         GameObject.__init__( self, imagem, posicao, velocidade )
         if lista != None:
             self.add( lista )
-    # __init__()
-# Tiro
 
 
 class Nave( GameObject ):
 
     vidas = None
 
-    def __init__( self, posicao, vidas=0, velocidade=[ 0, 0 ], imagem=None ):
+    def __init__( self, posicao, explosao, vidas=0, velocidade=[ 0, 0 ], imagem=None ):
         self.acceleration = [ 3, 3 ]
         GameObject.__init__( self, imagem, posicao, velocidade )
         self.set_vidas( vidas )
-        self.explosao = media.obter_som('explosao.wav')
-    # __init__()
+        self.explosao = explosao
 
     def get_vidas( self ):
         return self.vidas
-    # get_vidas()
 
     def set_vidas( self, vidas ):
         self.vidas = vidas
-    # set_vidas()
 
     def tiro( self, lista_tiros, imagem=None ):
         s = list( self.get_velocidade() )
         s[ 1 ] *= 2
         Tiro( self.get_posicao(), s, imagem, lista_tiros )
-    # tiro()
 
     def do_hit( self ):
         if self.get_vidas() == 0:
@@ -140,52 +119,42 @@ class Nave( GameObject ):
             self.explosao.play()
         else:
             self.set_vidas( self.get_vidas() - 1 )
-    # do_hit()
 
     def do_collision( self ):
         if self.get_vidas() == 0:
             self.kill()
         else:
             self.set_vidas( self.get_vidas() - 1 )
-    # do_collision()
 
     def is_dead( self ):
         return self.get_vidas() == 0
-    # is_dead()
 
     def accel_top( self ):
         velocidade = self.get_velocidade()
         self.set_velocidade( ( velocidade[ 0 ], velocidade[ 1 ] - self.acceleration[ 1 ] ) )
-    # accel_top
 
     def accel_bottom( self ):
         velocidade = self.get_velocidade()
         self.set_velocidade( ( velocidade[ 0 ], velocidade[ 1 ] + self.acceleration[ 1 ] ) )
-    # accel_bottom
 
     def accel_left( self ):        
         velocidade = self.get_velocidade()
         self.set_velocidade( ( velocidade[ 0 ] - self.acceleration[ 0 ], velocidade[ 1 ] ) )
-    # accel_left
 
     def accel_right( self ):
         velocidade = self.get_velocidade()
         self.set_velocidade( ( velocidade[ 0 ] + self.acceleration[ 0 ], velocidade[ 1 ] ) )
-    # accel_right
-# Nave
 
 
 class Inimigo( Nave ):
-    def __init__( self, posicao, vidas=0, velocidade=None, behaviour=0, imagem=None):
+    def __init__( self, posicao, explosao, vidas=0, velocidade=None, behaviour=0, imagem=None):
         if   behaviour == 0: # Inimigo normal, desce reto
             velocidade = (  0, 3 )
         elif behaviour == 1: # Inimigo que desce da esquerda pra direita
             velocidade = (  2, 3 )
         elif behaviour == 2: # Inimigo que desce da direita pra esquerda
             velocidade = ( -2, 3 )
-        Nave.__init__( self, posicao, vidas, velocidade, imagem )
-    # __init__()
-# Inimigo
+        Nave.__init__( self, posicao, explosao, vidas, velocidade, imagem )
 
 
 class Jogador( Nave ):
@@ -193,10 +162,9 @@ class Jogador( Nave ):
     A classe Jogador é uma classe derivada da classe GameObject.
     """
 
-    def __init__( self, posicao, vidas=10, imagem=None ):
-        Nave.__init__( self, posicao, vidas, [ 0, 0 ], imagem )
+    def __init__( self, posicao, explosao, vidas=10, imagem=None ):
+        Nave.__init__( self, posicao, explosao, vidas, [ 0, 0 ], imagem )
         self.set_XP( 0 )
-    # __init__()
 
     def update( self, dt ):
         velocidade_mov = ( self.velocidade[ 0 ] * dt / 16, self.velocidade[ 1 ] * dt / 16)
@@ -213,19 +181,15 @@ class Jogador( Nave ):
             
         elif ( self.rect.top < 0 ):
             self.rect.top = 0
-    # update()
     
     def get_posicao( self ):
         return ( self.rect.center[ 0 ], self.rect.top )
-    # get_posicao()
     
     def get_XP( self ):
         return self.XP
-    # get_XP()
 
     def set_XP( self, XP ):
         self.XP = XP
-    # get_XP()
 
     def tiro( self, lista_tiros, imagem=None ):
         l = 1
@@ -236,11 +200,9 @@ class Jogador( Nave ):
         velocidades = self.get_velocidade_tiro( l )
         for velocidade in velocidades:
             Tiro( posicao, velocidade, imagem, lista_tiros )
-    # tiro()
 
     def get_velocidade_tiro( self, municao ):
         velocidades = []
-
         if municao <= 0:
             return velocidades
         
@@ -260,5 +222,3 @@ class Jogador( Nave ):
             velocidades += [ (  4, -2 ) ]
 
         return velocidades
-    # get_velocidade_tiro()
-# Jogador
